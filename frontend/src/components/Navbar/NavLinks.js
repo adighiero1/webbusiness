91c1c4e70066f3508f8134d2b0c39aeb677f3067
Elpipi14 <107086659+Elpipi14@users.noreply.github.com>
2024-08-28 23:51:21 -0300

    modificaciones responsive

--- frontend/src/components/Navbar/NavLinks.js
+++ frontend/src/components/Navbar/NavLinks.js
@@ -4,14 +4,14 @@ import { HashLink } from 'react-router-hash-link';
 const NavLinks = () => {
     return (
         <>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#about">
-                About
+            <HashLink className="px-4 font-extrabold hover:text-blue-900" smooth to="/#about">
+            Sobre Nosotros
             </HashLink>
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" smooth to="/#services">
-                Services
+            <HashLink className="px-4 font-extrabold hover:text-blue-900" smooth to="/#services">
+            Servicios
             </HashLink>
-            <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" to="/contact#contact">
-                Contact Us
+            <HashLink className="bg-blue-900 transition hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" to="/contact#contact">
+            Contáctenos
             </HashLink>
         </>
     )
